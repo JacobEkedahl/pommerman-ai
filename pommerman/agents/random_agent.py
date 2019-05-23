32c7a2686b2b71716fb772074d71508ccc97ae65
JacobEkedahl <jacob.ekedahl@gmail.com>
2019-05-23 14:23:02 +0200

    some testing

--- pommerman/agents/random_agent.py
+++ pommerman/agents/random_agent.py
@@ -1,64 +1,11 @@
 '''An agent that preforms a random action each step'''
 from . import BaseAgent
 
-##########
-# B-tree #
-##########
-SUCCSESS = "SUCCSESS"
-RUNNING = "RUNNING"
-FAIL = "FAIL"
-
-SEQUENCE = "SEQUENCE"
-FALLBACK = "FALLBACK"
-
-class Node():
-  def __init__(self, func = None, status = None, typ = None, children = []):
-    self.func = func
-    self.status = status
-    self.typ = typ
-    self.children = children
-
-  def tick(self):
-    self.status=RUNNING
-    if self.typ==FALLBACK:
-      self.fallBack()
-    if self.typ == SEQUENCE:
-      self.sequence()
-    if self.typ == None:
-      s = self.func()
-      if s == True:
-        self.status=SUCCSESS
-      else:
-        self.status=FAIL
-
-  def fallBack(self):
-    for child in self.children:
-      child.tick()
-      if child.status==SUCCSESS:
-        self.status=SUCCSESS
-        return
-    self.status=FAIL
-    return
-
-  def sequence(self):
-    for child in self.children:
-      child.tick()
-      if child.status==FAIL:
-        self.status=FAIL
-        return
-    self.status=SUCCSESS
-    return
-
-root = Node(typ = FALLBACK)
-
 class RandomAgent(BaseAgent):
     """The Random Agent that returns random actions given an action_space."""
 
     def act(self, obs, action_space):
-        #print(action_space.sample())
-        #print(obs['board'])
-        #print(obs['bomb_blast_strength'])
-        #print(obs['enemies'])
-        #print(obs['ammo'])
-        print(obs['blast_strength'])
-        return action_space.sample()
+      print("hello")
+      print("ok")
+      print("hej")
+      return 0#action_space.sample()

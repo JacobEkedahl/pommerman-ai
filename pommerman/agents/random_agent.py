b4ac26a0706c61e3ecade85a35d22092a7a9f87d
JacobEkedahl <jacob.ekedahl@gmail.com>
2019-05-23 15:18:01 +0200

    collect valid positions

--- pommerman/agents/random_agent.py
+++ pommerman/agents/random_agent.py
@@ -1,5 +1,8 @@
 '''An agent that preforms a random action each step'''
 from . import BaseAgent
+import numpy as np
+from .. import constants
+from .. import utility
 
 STOP = 0
 UP = 1
@@ -57,33 +60,36 @@ class Node():
 
 class RandomAgent(BaseAgent):
     """The Random Agent that returns random actions given an action_space."""
+    def __init__(self, *args, **kwargs):
+        super(RandomAgent, self).__init__(*args, **kwargs)
+        self.root = Node(typ = FALLBACK)
+        self.buildTree()
 
     def act(self, obs, action_space):
+        self.collectObs(obs)
         #print(action_space)
         #print(obs['board'])
         #print(obs['bomb_blast_strength'])
         #print(obs['enemies'])
-        #print(obs['ammo'])
+
         #print(obs['blast_strength'])
-        self.root = Node(typ = FALLBACK)
-        self.buildTree()
         self.root.tick()
+        valid_positions = self.getValidPositions()
+        print(valid_positions)
         return self.action#LEFT#action_space.sample()
 
     ##########
-	# Build  #
-	##########
+  	# Build  #
+  	##########
     def buildTree(self):
         #self.root = Node(typ = FALLBACK)
         self.root.children = [Node(typ = SEQUENCE),Node(typ = SEQUENCE),Node(func = self.right)]
-
         self.root.children[0].children =[Node(func = self.false), Node(func = self.stop)]
         self.root.children[1].children =[Node(func = self.true), Node(func = self.left)]
 
-
-	##########
-	# Funcs  #
-	##########
+  	##############
+  	# Tree Funcs #
+  	##############
 
     def true(self):
         return True
@@ -97,3 +103,54 @@ class RandomAgent(BaseAgent):
         return True
     def right(self):
         self.action=RIGHT
+
+    ################
+    # Helper funcs #
+    ################
+    
+    def collectObs(self, obs):
+        self.obs = obs
+        self.my_position = tuple(obs['position'])
+        self.board = np.array(obs['board'])
+        self.bombs = self.convert_bombs(np.array(obs['bomb_blast_strength']))
+        self.enemies = [constants.Item(e) for e in obs['enemies']]
+        self.ammo = int(obs['ammo'])
+        self.blast_strength = int(obs['blast_strength'])
+        return
+
+    def convert_bombs(self, bomb_map):
+        '''Flatten outs the bomb array'''
+        ret = []
+        locations = np.where(bomb_map > 0)
+        for r, c in zip(locations[0], locations[1]):
+            ret.append({
+                'position': (r, c),
+                'blast_strength': int(bomb_map[(r, c)])
+            })
+        return ret
+
+    def getValidPositions(self):
+      # Choose a random but valid direction.
+        directions = [
+            constants.Action.Stop, constants.Action.Left,
+            constants.Action.Right, constants.Action.Up, constants.Action.Down
+        ]
+        valid_directions = self._filter_invalid_directions(
+            self.board,self.my_position, directions, self.enemies)
+        return valid_directions
+
+
+    ###############
+    # Static func #
+    ###############
+
+    @staticmethod
+    def _filter_invalid_directions(board, my_position, directions, enemies):
+        ret = []
+        for direction in directions:
+            position = utility.get_next_position(my_position, direction)
+            if utility.position_on_board(
+                    board, position) and utility.position_is_passable(
+                        board, position, enemies):
+                ret.append(direction)
+        return ret
